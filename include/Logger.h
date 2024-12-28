39acab77c1097601a440ecd13ee108dd8e77f2a5
Jens Langenberg <Jens.Langenberg@corsair.com>
2024-12-28 15:19:50 +0100

    Added facade header file

---
+++ include/Logger.h
@@ -0,0 +1,13 @@
+/*
+  ==============================================================================
+
+	Module			Logger
+	Description		Logger Factory Header to be included in other projects
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "LoggerWrapper.h"
+#include "PrintMacros.h"
